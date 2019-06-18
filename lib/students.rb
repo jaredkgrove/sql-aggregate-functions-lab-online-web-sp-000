e79089091535b85c4e5693b9de6b6a2f1bb8d78a
Jared Grove <jaredkgrove@gmail.com>
2019-06-18 21:27:48 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -6,12 +6,11 @@ end
 
 def lowest_student_gpa
   "SELECT MIN(gpa) FROM students"
-
 end
+
 
 def average_student_gpa
   "SELECT AVG(gpa) FROM students"
-
 end
 
 def total_tardies_for_all_students
@@ -19,3 +18,4 @@ end
 
 def average_gpa_for_9th_grade
 end
+
